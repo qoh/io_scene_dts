ee7d7845afececbcb58f92d84c57b1ec029fe955
port <portification@gmail.com>
2015-06-22 17:37:57 +0200

    Import materials

--- import_dts.py
+++ import_dts.py
@@ -172,6 +172,56 @@ def write_debug_report(filepath, shape):
         for i, name in enumerate(shape.names):
             p("  " + str(i) + " = " + name)
 
+from random import random
+
+default_materials = {
+    "black": (0, 0, 0),
+    "black25": (191, 191, 191),
+    "black50": (128, 128, 128),
+    "black75": (64, 64, 64),
+    "blank": (255, 255, 255),
+    "blue": (0, 0, 255),
+    "darkRed": (128, 0, 0),
+    "gray25": (64, 64, 64),
+    "gray50": (128, 128, 128),
+    "gray75": (191, 191, 191),
+    "green": (26, 128, 64),
+    "lightBlue": (10, 186, 245),
+    "lightYellow": (249, 249, 99),
+    "palegreen": (125, 136, 104),
+    "red": (213, 0, 0),
+    "white": (255, 255, 255),
+    "yellow": (255, 255, 0)
+}
+
+for name, color in default_materials.items():
+    default_materials[name] = (color[0] / 255, color[1] / 255, color[2] / 255)
+
+def import_material(dmat):
+    bmat = bpy.data.materials.new(dmat.name)
+
+    if False:
+        pass # did we find a texture?
+    elif dmat.name in default_materials:
+        bmat.diffuse_color = default_materials[dmat.name]
+    else: # give it a random color
+        bmat.diffuse_color = (random(), random(), random())
+
+    if dmat.flags & Material.Translucent:
+        bmat.use_transparency = True
+    if dmat.flags & Material.SelfIlluminating:
+        bmat.use_shadeless = True
+    if dmat.flags & Material.Additive:
+        bmat["additive"] = True
+    if dmat.flags & Material.Subtractive:
+        bmat["subtractive"] = True
+    if not (dmat.flags & Material.SWrap):
+        bmat["noSWrap"] = True
+    if not (dmat.flags & Material.TWrap):
+        bmat["noTWrap"] = True
+
+    return bmat
+
 def load(operator, context, filepath,
          lod_as_empty=True,
          include_armatures=True,
@@ -186,6 +236,7 @@ def load(operator, context, filepath,
         write_debug_report(filepath + ".txt", shape)
 
     node_lod = {}
+    scene_material_table = {}
 
     if lod_as_empty:
         for lod in shape.detail_levels:
@@ -247,8 +298,21 @@ def load(operator, context, filepath,
 
         faces = []
 
+        # Create a new mesh, update data later
+        bmesh = bpy.data.meshes.new(name="Mesh")
+
+        mesh_material_table = {}
+        mesh_material_apply = {}
+
         # Go through all the primitives in the mesh and convert them to pydata faces
         for prim in mesh.primitives:
+            material_dts = None
+
+            if not (prim.type & Primitive.NoMaterial):
+                material_dts = shape.materials[prim.type & Primitive.MaterialMask]
+
+            apply_start = len(faces)
+
             if prim.type & Primitive.Strip:
                 even = True
                 for i in range(prim.firstElement + 2, prim.firstElement + prim.numElements):
@@ -269,10 +333,44 @@ def load(operator, context, filepath,
                 for i in range(prim.firstElement + 2, prim.firstElement + prim.numElements, 3):
                     faces.append((mesh.indices[i], mesh.indices[i - 1], mesh.indices[i - 2]))
 
-        # Now add the Mesh object and parent it to the armature if any
-        bmesh = bpy.data.meshes.new(name="Mesh")
+            apply_end = len(faces)
+
+            if apply_end > apply_start and material_dts:
+                material = scene_material_table.get(material_dts)
+
+                if material == None:
+                    material = import_material(material_dts)
+                    scene_material_table[material_dts] = material
+
+                material_index = mesh_material_table.get(material)
+
+                if material_index == None:
+                    material_index = len(bmesh.materials)
+                    mesh_material_table[material] = material_index
+                    bmesh.materials.append(material)
+
+                for i in range(apply_start, apply_end):
+                    mesh_material_apply[i] = material_index
+
+        # Now add faces & vertices and parent it to the armature if any
         bmesh.from_pydata(mesh.verts, [], faces)
         bmesh.update()
+
+        # Assign all the materials first
+        if mesh_material_apply:
+            faces_no_mat = set(range(len(bmesh.polygons)))
+
+            for face_index, material_index in mesh_material_apply.items():
+                bmesh.polygons[face_index].material_index = material_index
+                faces_no_mat.remove(face_index)
+
+            if faces_no_mat:
+                index_no_mat = len(bmesh.materials)
+                bmesh.materials.append(None)
+
+                for face_index in faces_no_mat:
+                    bmesh.polygons[face_index].material_index = index_no_material
+
         bobj = bpy.data.objects.new(name=shape.names[obj.name], object_data=bmesh)
         context.scene.objects.link(bobj)
 
@@ -282,8 +380,7 @@ def load(operator, context, filepath,
             else:
                 objects.setdefault(obj.node, []).append(bobj)
 
-        if hide_default_player and shape.names[obj.name] not in (
-            "HeadSkin", "chest", "Larm", "Lhand", "Rarm", "Rhand", "pants", "LShoe", "RShoe"):
+        if hide_default_player and shape.names[obj.name] not in ("HeadSkin", "chest", "Larm", "Lhand", "Rarm", "Rhand", "pants", "LShoe", "RShoe"):
             bobj.hide = True
 
     if not include_armatures:
